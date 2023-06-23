06f29987e58dcb56ceb61b56320a8489bcc826fd
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2023-06-23 20:41:28 +0600

    feat: testing the return type of methods;

---
+++ MSTestProject.NUnitTests/CustomerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using MSTestProject.Fundamentals;
+using static MSTestProject.Fundamentals.CustomerController;
+
+namespace MSTestProject.NUnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void GetCustomer_WhenCalled_ReturnNotFound(int id) 
+        {
+            var result = _customerController.GetCustomer(id);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsGreaterThanZero_ReturnOk()
+        {
+            var result = _customerController.GetCustomer(3);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
