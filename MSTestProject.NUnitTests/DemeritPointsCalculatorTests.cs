71f6669b2a639552a3a19a4acbb28a353542b2dc
Nurdaulet Khozhakeldiyev <thekazman@outlook.com>
2023-07-06 23:06:51 +0600

    feat: some exercises;

---
+++ MSTestProject.NUnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using MSTestProject.Fundamentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestProject.NUnitTests
+{
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _calculator;
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(302)]
+        public void CalculateDemeritPoints_WhenSpeedIsIncreadible_ThrowArgOutOfRangeExp(int speed)
+        {
+            Assert.That(() => _calculator.CalculateDemeritPoints(speed), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(30)]
+        [TestCase(65)]
+        public void CalculateDemeritPoints_WhenSpeedIsNotExceeded_ReturnsZero(int speed)
+        {
+            var result = _calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_WhenSpeedIsExceeded_ReturnsDemeritPoint()
+        {
+            var result = _calculator.CalculateDemeritPoints(75);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ MSTestProject.NUnitTests/StackTests.cs
@@ -0,0 +1,91 @@
+﻿namespace MSTestProject.NUnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Fundamentals.Stack<string> _stack;
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Fundamentals.Stack<string>();
+        }
+
+        [Test]
+        public void Push_NullArgument_ThrowsArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArgument_AddTheObjectToTheStack()
+        {
+            _stack.Push("1");
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_StaskIsEmpty_ReturnsZero()
+        {
+            var result = _stack.Count;
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_StackIsEmpty_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackContainsFewObjects_ReturnsObjestOnTheTop()
+        {
+            _stack.Push("1");
+            _stack.Push("2");
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Pop_StackContainsFewObjects_RemovesObjestOnTheTop()
+        {
+            _stack.Push("1");
+            _stack.Push("2");
+            
+            _stack.Pop();
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Peek_WhenStackIsEmpty_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackContainsFewObject_ReturnsObjestOnTheTop()
+        {
+            _stack.Push("1");
+            _stack.Push("2");
+
+            var result = _stack.Peek();
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Peek_StackContainsFewObject_DoestNotRemoveObjectOnTheTop()
+        {
+            _stack.Push("1");
+            _stack.Push("2");
+
+            _stack.Peek();
+
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+    }
+}
